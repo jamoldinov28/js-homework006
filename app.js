a484206fc723be17ddeacff632b1749300422b03
jamoldinov28 <jamoldinovvhalilillo@gmail.com>
2024-11-05 11:45:12 +0500

    masalalar

---
+++ app.js
@@ -0,0 +1,125 @@
+// 1-masala                                                            
+{                                                            
+    let a = 15                                                            
+    let b = 4                                                            
+    while(a >= b){                                                            
+        a = a - b                                                             
+    }                                                            
+    console.log(a);                                                            
+}                                                            
+                                                            
+                                                            
+// 2-masala                                                            
+{                                                            
+    let a = 15                                                            
+    let b = 4                                                            
+    let s = 0                                                            
+    while (a >= b){                                                            
+        a = a - b                                                            
+        s++                                                            
+    }                                                            
+    console.log(s);                                                            
+}                                                            
+                                                            
+                                                            
+// 3-masala                                                            
+{                                                            
+    let n = 17                                                            
+    let k = 5                                                            
+    let b = 0   //butun qismi                                                            
+    while(n >= k){                                                            
+        n -= k                                                            
+        b++                                                            
+    }                                                            
+    console.log("Butun qismi ", b);                                                            
+                                                            
+    console.log("Qoldiq ", n);                                                            
+}                                                            
+                                                            
+                                                            
+// 4-masala                                                            
+{                                                            
+    let n = 9                                                            
+    let a = 3                                                            
+    while(n >= 2){                                                            
+        n = n -  a                                                            
+    }                                                            
+   console.log(` ${a} - ning darajasi`);                                                            
+}                                                            
+                                                            
+                                                            
+// 5-masala                                                            
+{                                                            
+    let n = 4                                                            
+    let k = 0                                                            
+    while(n >= 2){                                                            
+        n /= 2                                                            
+        k++                                                            
+    }                                                            
+    console.log(k);                                                            
+}                                                            
+                                                            
+                                                            
+// 6-masala                                                            
+{                                                            
+    let n = 8                                                            
+    let a = 1                                                            
+    while (n > 1){                                                            
+      a *= n                                                            
+      n -= 2                                                            
+    }                                                            
+    console.log(a);                                                            
+                                                            
+}                                                            
+                                                            
+// 7-masala                                                            
+{                                                            
+  let n = 20                                                            
+  let k = 0                                                            
+                                                              
+  while (k * k <= n){                                                            
+    k++;                                                             
+  }                                                            
+                                                              
+  console.log(k);                                                             
+                                                            
+}                                                            
+                                                            
+                                                            
+// 8-masala                                                            
+{                                                            
+    let n = 20                                                            
+    let k = 1                                                            
+                                                                
+    while ((k + 1) * (k + 1) <= n){                                                            
+      k++                                                            
+    }                                                            
+    console.log(k);                                                            
+}                                                            
+                                                            
+                                                            
+// 9-masala                                                            
+{                                                            
+    let n = 15                                                            
+    let k = 1                                                            
+                                                            
+    while (3 * k <= n){                                                            
+        k++                                                            
+    }                                                            
+                                                            
+    console.log(k);                                                            
+}                                                            
+                                                            
+                                                            
+// 10-masala                                                            
+{                                                            
+    let n = 28                                                            
+    let k = 0                                                            
+                                                                
+    while (3 * (k + 1) <= n){                                                            
+        k++                                                            
+    }                                                            
+                                                                
+    console.log(k);                                                            
+                                                                
+}                                                            
